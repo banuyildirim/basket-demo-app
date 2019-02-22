3cba261bf4f636c549fdd6951d8b1fca5f53da42
banuyildirim <banuyildirim17@gmail.com>
2019-02-22 19:24:40 +0300

    json data html template

--- app/main.js
+++ app/main.js
@@ -1,14 +1,6 @@
 //main.js
 
-import data from '../json/menu.json';
+import './html-template';
 
-const $obj = {
-    category: ''
-}
 
-for (let i = 0; i < data.d.TotalRowCount; i++) {
-    $obj.category += data.d.ResultSet[i].CategoryDisplayName;
-}
-
-console.log($obj.category);
 
