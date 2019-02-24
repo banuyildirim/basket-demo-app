24ffc456e1dc3dd406c6edeb5a518bf04f982c7a
banuyildirim <banuyildirim17@gmail.com>
2019-02-24 18:34:14 +0300

    add to basket

--- style/basket/basket.js
+++ style/basket/basket.js
@@ -3,18 +3,42 @@ import data from '../../json/menu.json';
 const states = {
     result: data.d.ResultSet
 }
-let btnAddBasket = document.getElementsByClassName('js-add-product');
-const productsArray = [];
-states.result.map((value) => {
-    productsArray.push(value.Products);
-});
-console.log(productsArray);
+const $obj = {
+    btnAddBasket: document.getElementsByClassName('js-add-product')
+}
 
-Object.entries(btnAddBasket).map(( object ) => {
+const productsArray = new Array();
+const basketArray = new Array();
+
+states.result.map((value) => {
+    for (let i = 0; i < value.Products.length; i++) {
+        productsArray.push(value.Products[i]);
+    }
+});
+
+function filterViaCategory(arr, category) {
+    return arr.filter((obj) => {
+        if (obj.ProductId === category) {
+            return true;
+        }
+        return false;
+    });
+}
+
+const tableTemplate = (values) => `<table border='1' width='100%'><tr>
+    ${ values.map(value => 
+    `<td align='center'>${value.DisplayName} </td>`
+    ).join(' ') }</tr></table>`;
+
+document.getElementsByClassName('items').innerText = "";
+
+Object.entries($obj.btnAddBasket).map(( object ) => {
     object[1].addEventListener("click", function() {
-        const res2 = productsArray.filter((item) => {
-            return item.ProductId === this.dataset.id;
-        });
-        console.log(res2);
+        basketArray.push((filterViaCategory(productsArray, this.dataset.id)));
+        console.log(basketArray);
+        setTimeout(() => {
+            document.getElementsByClassName('items').innerText = tableTemplate(basketArray);
+        }, 3000);
     });
 });
+
--- style/product/product-template.js
+++ style/product/product-template.js
@@ -12,7 +12,7 @@ const states = {
 const htmlTemplate = (values) => `<article>
     ${ values.map(value => 
         `<div class='product' id=${value.Oid}><div class='product__head'> <h2>${value.CategoryDisplayName} </h2></div><div class='product__content'>
-        <ul>${value.Products.map(el => `<li><div class='product__actions'><input type='text' class='product__count' data-id=${el.ProductId} value='1' /><button class='btn-primary js-add-product' type='button' data-id=${el.ProductId}>+</button></div><div class='product__info'> <div class='product__info--middle' data-id=${el.ProductId}>${el.DisplayName}</div> <div class='product__info--small'>${el.Description}</div></div><div class='product__price'>${el.ExtendedPrice}</div></li>`
+        <ul>${value.Products.map(el => `<li><div class='product__actions'><input type='text' class='product__count' data-id=${el.ProductId} value='1' /><button class='btn-primary js-add-product' type="click" data-id=${el.ProductId}>+</button></div><div class='product__info'> <div class='product__info--middle' data-id=${el.ProductId}>${el.DisplayName}</div> <div class='product__info--small'>${el.Description}</div></div><div class='product__price'>${el.ExtendedPrice}</div></li>`
         ).join(' ')}</ul>
         </div></div>`
     ).join(' ') }</article>`;
